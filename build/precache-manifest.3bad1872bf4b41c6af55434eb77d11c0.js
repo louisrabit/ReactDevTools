20a75540b3256b38dcfca81fa9839fcd7697e869
Luis Manuel Brito Coelho <luiscoelho18994@gmail.com>
2023-02-10 23:14:03 +0000

    Update 14 files

---
+++ build/precache-manifest.3bad1872bf4b41c6af55434eb77d11c0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e5658cc5b6f99dc8a5aa96f3abb8083",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f582bf181655e4c3e86",
+    "url": "/static/js/2.aefe00b0.chunk.js"
+  },
+  {
+    "revision": "9c9b5d9948e9dff66be3",
+    "url": "/static/js/main.d1efb39a.chunk.js"
+  },
+  {
+    "revision": "7ebe2cd53892b00c3fe6",
+    "url": "/static/js/runtime-main.9203f476.js"
+  }
+]);
